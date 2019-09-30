11c6a537c84a3a5ec8bd5dc0d03c5d79fc185e88
mcameriere <31536331+mcameriere@users.noreply.github.com>
2019-09-30 22:00:45 +0200

    Update SimpleFragment.java

--- SimpleFragment.java
+++ SimpleFragment.java
@@ -11,8 +11,7 @@ import androidx.fragment.app.Fragment;
 public class SimpleFragment extends Fragment implements Backable {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_simple, container, false);
     }
 
